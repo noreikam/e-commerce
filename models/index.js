c499fbe0e79754f103c7a10d00acebd156d7c597
noreikam <noreikam1@gmail.com>
2022-05-08 23:35:01 -0400

    product routes built and tested

--- models/index.js
+++ models/index.js
@@ -16,14 +16,14 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'tagged_product',
+  as: 'product_tag',
   foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'product_tag',
+  as: 'tag_product',
   foreignKey: 'tag_id'
 })
 
